672344a172dd4b2237ad658f4eeccb3bb73102b1
Rishi-Kant <kant3397@gmail.com>
2023-12-26 21:32:04 +0530

    added tech stack

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import Layout from './components/layout/Layout'
 import About from './pages/about/About'
 import TechStack from './pages/teckStack/TechStack'
+import Projects from './pages/projects/Projects'
 function App() {
   return (
     <div>
@@ -11,7 +12,7 @@ function App() {
       <About/>
       </div>
       <TechStack/>
-      
+      <Projects/>
     </div>
   )
 }
